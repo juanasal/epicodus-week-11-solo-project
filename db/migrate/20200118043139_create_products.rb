fff916c6b6c05ea07fddcdfc46feb18058050240
jonathansalfen <jonathan.salfen@gmail.com>
2020-01-17 21:07:30 -0800

    add tables

--- db/migrate/20200118043139_create_products.rb
+++ db/migrate/20200118043139_create_products.rb
@@ -1,6 +1,12 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
+      t.decimal :cost
+      t.string :country_of_origin
+      t.string :grocery_category
+      t.string :name
+
+      t.timestamps()
     end
   end
 end
--- db/migrate/20200118043151_create_reviews.rb
+++ db/migrate/20200118043151_create_reviews.rb
@@ -1,6 +1,12 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
+  # validates_length_of :rating, in: 0..5, allow_nil: false, allow_blank: false
   def change
     create_table :reviews do |t|
+      t.string :author
+      t.string :content_body
+      t.integer :rating
+
+      t.timestamps()
     end
   end
 end
