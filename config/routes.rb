14ab975083b055e5c843419bf6d0c06559db8de5
jonathansalfen <jonathan.salfen@gmail.com>
2020-01-19 11:28:27 -0800

    add nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products
+  root to: 'products#index'
+
+  resources :products do
+    resources :ratings
+  end
 end
