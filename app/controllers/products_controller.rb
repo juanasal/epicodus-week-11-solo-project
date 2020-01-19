a5b55addab90c4b4258335f5c8b9612db6c3deb9
jonathansalfen <jonathan.salfen@gmail.com>
2020-01-19 12:17:12 -0800

    add controller actions for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,31 +1,59 @@
 class ProductsController < ApplicationController
 
   def index
-    # Code for listing all albums goes here.
+    # Code for listing all products goes here.
+    @products = Product.all
+    render :index
   end
 
   def new
-    # Code for new album form goes here.
+    # Code for new product form goes here.
+    @product = Product.new
+    render :new
   end
 
   def create
-    # Code for creating a new album goes here.
+    # Code for creating a new product goes here.
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
 
   def edit
-    # Code for edit album form goes here.
+    # Code for edit product form goes here.
+    @product = Product.find(params[:id])
+    render :edit
   end
 
   def show
-    # Code for showing a single album goes here.
+    # Code for showing a single product goes here.
+    @product = Product.find(params[:id])
+    render :show
   end
 
   def update
-    # Code for updating an album goes here.
+    # Code for updating an product goes here.
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-    # Code for deleting an album goes here.
+    # Code for deleting an product goes here.
+    @product = Product.find(params[:id])
+    @product.destory
+    redirect_to products_path
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:cost, :country_of_origin, :grocery_category, :name)
   end
 
 end
