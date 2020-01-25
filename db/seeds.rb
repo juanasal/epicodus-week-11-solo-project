10d5577c08a55d6f9f90bf8f88ae9db31ac901a9
jonathansalfen <jonathan.salfen@gmail.com>
2020-01-25 09:12:41 -0800

    seed database using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(cost: rand(100), country_of_origin: Faker::Address.country, grocery_category: Faker::Restaurant.type, name: Faker::Food.ingredient)
+end
+
+p "Created #{Product.count} products."
