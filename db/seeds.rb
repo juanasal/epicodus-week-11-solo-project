6dc2c317f7ee9ed8b48e9a15d31864602be526b9
jonathansalfen <jonathan.salfen@gmail.com>
2020-01-25 13:58:40 -0800

    seed database with both products and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.destroy_all
+Review.destroy_all
 
 50.times do |index|
   Product.create!(cost: rand(100), country_of_origin: Faker::Address.country, grocery_category: Faker::Restaurant.type, name: Faker::Food.ingredient)
 end
 
+products = Product.all
+product_ids = []
+products.each do |product|
+  product_ids.push(product.id)
+end
+
+
+500.times do |index|
+  Review.create!(author: Faker::Name.unique.name, content_body: Faker::Restaurant.review, rating: rand(6), product_id: product_ids[rand(product_ids.length)])
+end
+
 p "Created #{Product.count} products."
+p "Created #{Review.count} reviews."
