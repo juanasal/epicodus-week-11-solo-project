56b4b852c4e1e2a90ab3e82b00649b7cb00cdb6b
jonathansalfen <jonathan.salfen@gmail.com>
2020-01-26 13:36:48 -0800

    add product and review validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,15 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  validates :name, presence:true
-  validates_length_of :name, maximum: 100
+  validates :cost, presence:true
+  validates :country_of_origin, presence:true, length: { maximum: 100}
+  validates :grocery_category, presence:true, length: { maximum: 100}
+  validates :name, presence:true, length: { maximum: 100}
   before_save(:titelize_product)
 
   private
     def titelize_product
       self.name = self.name.titleize
       self.country_of_origin = self.country_of_origin.titleize
-      self.grocery_category = grocery_category.titleize
+      self.grocery_category = self.grocery_category.titleize
     end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, presence: true, length: { maximum: 100 }
+  validates :content_body, presence: true, length: { minimum: 50, maximum: 250 }
+  validates :rating, presence: true
+  validates :product_id, presence: true
 end
