b65a4049f82e8fe0c609348e6b5111c41e881913
jonathansalfen <jonathan.salfen@gmail.com>
2020-01-25 19:30:03 -0800

    fix testing error

---
+++ spec/features/product_pages_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe "the add a product process" do
+  it "adds a new product" do
+    
+  end
+
+
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'capybara/rails'
+# require 'rspec/rails'
+# require 'capybara/rails'
+# lesson says to have the above two lines, but they are causing errors.  
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
