bbd81e0db8738be2cfbef022eec563f310ab7f28
jonathansalfen <jonathan.salfen@gmail.com>
2020-01-26 14:13:58 -0800

    add validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :product
   validates :author, presence: true, length: { maximum: 100 }
   validates :content_body, presence: true, length: { minimum: 50, maximum: 250 }
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: {less_than: 6, greater_than: 0}
   validates :product_id, presence: true
 end
