95068babd718d80d4d48a3127c1bc31f53b7a157
jonathansalfen <jonathan.salfen@gmail.com>
2020-01-26 19:45:02 -0800

    update with user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_18_061749) do
+ActiveRecord::Schema.define(version: 2020_01_27_032607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2020_01_18_061749) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "product_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "reviews", "products"
